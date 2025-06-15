6e9abdffdce305080e9f1b9cec55405860240651
RizalAhmadMaulana <rizalram100@gmail.com>
2025-06-15 19:31:34 +0700

    menambahkan data transaksi pembelian

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -31,6 +31,12 @@ $routes->group('keranjang', ['filter' => 'auth'], function ($routes) {
     $routes->get('clear', 'TransaksiController::cart_clear');
 });
 
+
+$routes->get('checkout', 'TransaksiController::checkout', ['filter' => 'auth']);
+$routes->get('get-location', 'TransaksiController::getLocation', ['filter' => 'auth']);
+$routes->get('get-cost', 'TransaksiController::getCost', ['filter' => 'auth']);
+$routes->post('buy', 'TransaksiController::buy', ['filter' => 'auth']);
+
 // Untuk Login & Logout
 $routes->get('/', 'AuthController::login');
 $routes->get('login', 'AuthController::login');
